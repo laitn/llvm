f5adb8fdb05e4588e5efd63e393e3974f470e108
Xiaochu Liu <xiaochu1122@gmail.com>
2015-11-05 14:41:21 -0800

    add DESLECT pass.

---
+++ lib/Transforms/Approx/DE_SELECT.cpp
@@ -0,0 +1,53 @@
+//===- DESELECT.cpp - Code for operator strength reduction ---------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements removal of 'select' instruction.
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Transforms/Approx.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "deselect"
+
+STATISTIC(OpCounter, "Counts number of select removed.");
+
+namespace {
+  struct DESELECT : public FunctionPass {
+    static char ID; // Pass identification, replacement for typeid
+    DESELECT() : FunctionPass(ID) {
+      initializeDESELECTPass(*PassRegistry::getPassRegistry());
+    }
+
+    bool runOnFunction(Function &F) override ;
+  };
+}
+
+char DESELECT::ID = 0;
+INITIALIZE_PASS(DESELECT, "deselect", "Remove select instructions.", false, false)
+
+Pass *llvm::createDESELECTPass() {
+  return new DESELECT();
+}
+
+
+bool DESELECT::runOnFunction(Function &Fn){
+  for (Function::iterator BB = Fn.begin(); BB != Fn.end(); ++BB) {
+    for(BasicBlock::iterator I=BB->begin();I!=BB->end();){
+    }
+  }
+  return false;
+}
