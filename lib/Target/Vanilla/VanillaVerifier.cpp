a1691c689573170f24c2bdd32f8a391a6e6f05a1
Xiaochu Liu <xiaochu1122@gmail.com>
2015-11-23 17:24:24 -0800

    Optimize verifier.

--- lib/Target/Vanilla/VanillaVerifier.cpp
+++ lib/Target/Vanilla/VanillaVerifier.cpp
@@ -45,25 +45,19 @@ namespace {
       return "Vanilla verification pass.";
     }
 
-    bool runOnMachineBasicBlock0(MachineBasicBlock &MBB);
-    bool runOnMachineBasicBlock1(MachineBasicBlock &MBB);
-    bool runOnMachineBasicBlock2(MachineBasicBlock &MBB);
+    bool runOnMachineBasicBlock_addwait(MachineBasicBlock &MBB);
+    bool runOnMachineBasicBlock_regalloc(MachineBasicBlock &MBB);
     bool runOnMachineFunction(MachineFunction &F) override {
       bool Changed = false;
       Subtarget = &F.getSubtarget<VanillaSubtarget>();
-      // 0th pass -- check two-address + R1 dst.
       for (MachineFunction::iterator FI = F.begin(), FE = F.end();
            FI != FE; ++FI)
-        Changed |= runOnMachineBasicBlock0(*FI);
-      // 1st pass -- 2-address form.
-      for (MachineFunction::iterator FI = F.begin(), FE = F.end();
-           FI != FE; ++FI)
-        Changed |= runOnMachineBasicBlock1(*FI);
+        Changed |= runOnMachineBasicBlock_regalloc(*FI);
       // 2nd pass -- add program terminator to signal hardware.
       if(F.getName().compare("main")==0){
         for (MachineFunction::iterator FI = F.begin(), FE = F.end();
              FI != FE; ++FI)
-          Changed |= runOnMachineBasicBlock2(*FI);
+          Changed |= runOnMachineBasicBlock_addwait(*FI);
       }
       return Changed;
     }
@@ -75,86 +69,13 @@ FunctionPass *llvm::createVanillaVerifierPass(TargetMachine &tm) {
   return new VanillaVerifier(tm);
 }
 
-bool VanillaVerifier::runOnMachineBasicBlock1(MachineBasicBlock &MBB) {
+bool VanillaVerifier::runOnMachineBasicBlock_regalloc(MachineBasicBlock &MBB){
   bool Changed = false;
-  Subtarget = &MBB.getParent()->getSubtarget<VanillaSubtarget>();
-  const TargetInstrInfo *TII = Subtarget->getInstrInfo();
-  for (MachineBasicBlock::iterator I = MBB.begin(); I != MBB.end(); ) {
-    MachineBasicBlock::iterator MI = I;
-    if(MI->getNumOperands()==3){
-      if(MI->getOperand(0).isReg()
-         && MI->getOperand(1).isReg()
-         && MI->getOperand(2).isReg()){
-        if(MI->getOperand(0).getReg()==MI->getOperand(1).getReg()){
-          // correct and do nothing;
-        }
-        else{
-          // rd = rds op rs
-          // --->
-          // mov r1, rds
-          // r1 =r1, rs
-          // mov rd, r1
-          assert(I!=MBB.end() && "insert point is end().0");
-          BuildMI(MBB, I, I->getDebugLoc(), TII->get(Vanilla::MOV), Vanilla::R1).addReg(MI->getOperand(1).getReg());
-          I++;
-          assert(
-                 (MI->getOperand(0).getReg()!=Vanilla::R1
-                  && MI->getOperand(1).getReg()!=Vanilla::R1
-                  && MI->getOperand(2).getReg()!=Vanilla::R1)
-                 && "R1 is already used.");
-          if(I!=MBB.end()){
-            BuildMI(MBB, I, I->getDebugLoc(), TII->get(Vanilla::MOV), MI->getOperand(0).getReg()).addReg(Vanilla::R1);
-          }
-          else{
-            BuildMI(&MBB, MI->getDebugLoc(), TII->get(Vanilla::MOV), MI->getOperand(0).getReg()).addReg(Vanilla::R1);
-          }
-          MI->getOperand(0).setReg(Vanilla::R1);
-          MI->getOperand(1).setReg(Vanilla::R1);
-          transform++;
-          Changed=true;
-          continue;
-        }
-      }
-      else if(MI->getOperand(0).isReg()
-              && MI->getOperand(1).isReg()
-              && MI->getOperand(2).isImm()){
-        // appi r1,rx, imm
-        // --->
-        // mov  r1,rx
-        // appi r1,r1, imm
-        if(MI->getOperand(0).getReg()!=MI->getOperand(1).getReg()){
-          BuildMI(MBB, I, I->getDebugLoc(), TII->get(Vanilla::MOV), Vanilla::R1).addReg(MI->getOperand(1).getReg());
-          MI->getOperand(1).setReg(Vanilla::R1);
-          I++;
-          transform++;
-          Changed=true;
-          continue;
-        }
-      }
-    }
-    I++;
-  }
-  return Changed;
-}
-
-bool VanillaVerifier::runOnMachineBasicBlock2(MachineBasicBlock &MBB) {
-  const TargetInstrInfo *TII = Subtarget->getInstrInfo();
-  for (MachineBasicBlock::iterator I = MBB.begin(); I != MBB.end(); I++) {
-    if(I->getOpcode()==Vanilla::JALR && I->getOperand(0).isReg() && I->getOperand(0).getReg()==Vanilla::R2){
-      BuildMI(MBB, I, I->getDebugLoc(), TII->get(Vanilla::WAIT), Vanilla::R0).addReg(Vanilla::R0);
-    }
-  }
-  return true;
-}
-
-bool VanillaVerifier::runOnMachineBasicBlock0(MachineBasicBlock &MBB) {
-  bool Changed = false;
-  Subtarget = &MBB.getParent()->getSubtarget<VanillaSubtarget>();
   const TargetInstrInfo *TII = Subtarget->getInstrInfo();
   for (MachineBasicBlock::iterator I = MBB.begin(); I != MBB.end(); ) {
     MachineBasicBlock::iterator MI = I;
     if (MI->getOpcode()==Vanilla::MOVI && MI->getOperand(0).getReg()!=Vanilla::R1){
-      // re-allocate register using reserved R1.
+      //movi rx, imm
       I++;
       if(I!=MBB.end()){
         BuildMI(MBB, I, I->getDebugLoc(), TII->get(Vanilla::MOV), MI->getOperand(0).getReg()).addReg(Vanilla::R1);
@@ -168,11 +89,11 @@ bool VanillaVerifier::runOnMachineBasicBlock0(MachineBasicBlock &MBB) {
       continue;
     }
     else if ((MI->getOpcode()==Vanilla::BEQZ
-             || MI->getOpcode()==Vanilla::BNEQZ
-             || MI->getOpcode()==Vanilla::BGTZ
-             || MI->getOpcode()==Vanilla::BLTZ)
+              || MI->getOpcode()==Vanilla::BNEQZ
+              || MI->getOpcode()==Vanilla::BGTZ
+              || MI->getOpcode()==Vanilla::BLTZ)
              && MI->getOperand(0).getReg()!=Vanilla::R1){
-      // re-allocate register using reserved R1.
+      //bxxz rx, mbb
       BuildMI(MBB, I, I->getDebugLoc(), TII->get(Vanilla::MOV), Vanilla::R1).addReg(MI->getOperand(0).getReg());
       MI->getOperand(0).setReg(Vanilla::R1);
       transform++;
@@ -180,17 +101,73 @@ bool VanillaVerifier::runOnMachineBasicBlock0(MachineBasicBlock &MBB) {
       Changed=true;
       continue;
     }
-    else if(MI->getOpcode()==Vanilla::APPI && MI->getOperand(0).getReg()!=Vanilla::R1){
-      // re-allocate register using reserved R1.
+    else if(MI->getNumOperands()==3
+         && MI->getOperand(0).isReg()
+         && MI->getOperand(1).isReg()
+         && MI->getOperand(2).isReg()
+         && MI->getOperand(0).getReg()!=MI->getOperand(1).getReg()){
+      // rd = rds op rs
+      // --->
+      // mov r1, rds
+      // r1 =r1, rs
+      // mov rd, r1
+      BuildMI(MBB, I, I->getDebugLoc(), TII->get(Vanilla::MOV), Vanilla::R1).addReg(MI->getOperand(1).getReg());
       I++;
-      BuildMI(MBB, I, I->getDebugLoc(), TII->get(Vanilla::MOV), MI->getOperand(0).getReg()).addReg(Vanilla::R1);
+      assert(
+             (MI->getOperand(0).getReg()!=Vanilla::R1
+              && MI->getOperand(1).getReg()!=Vanilla::R1
+              && MI->getOperand(2).getReg()!=Vanilla::R1)
+             && "R1 is already used.");
+      if(I!=MBB.end()){
+        BuildMI(MBB, I, I->getDebugLoc(), TII->get(Vanilla::MOV), MI->getOperand(0).getReg()).addReg(Vanilla::R1);
+      }
+      else{
+        BuildMI(&MBB, MI->getDebugLoc(), TII->get(Vanilla::MOV), MI->getOperand(0).getReg()).addReg(Vanilla::R1);
+      }
       MI->getOperand(0).setReg(Vanilla::R1);
+      MI->getOperand(1).setReg(Vanilla::R1);
       transform++;
       Changed=true;
       continue;
     }
-    I++;
+    else if(MI->getOpcode()==Vanilla::APPI
+            && (MI->getOperand(0).getReg()!=Vanilla::R1 || MI->getOperand(1).getReg()!=Vanilla::R1)){
+      // appi ry,rx, imm
+      // --->
+      // mov  r1,rx
+      // appi r1,r1, imm
+      // mov  ry,r1
+      BuildMI(MBB, I, I->getDebugLoc(), TII->get(Vanilla::MOV), Vanilla::R1).addReg(MI->getOperand(1).getReg());
+      I++;
+      assert(
+             (MI->getOperand(0).getReg()!=Vanilla::R1
+              && MI->getOperand(1).getReg()!=Vanilla::R1)
+             && "R1 is already used.");
+      if(I!=MBB.end()){
+        BuildMI(MBB, I, I->getDebugLoc(), TII->get(Vanilla::MOV), MI->getOperand(0).getReg()).addReg(Vanilla::R1);
+      }
+      else{
+        BuildMI(&MBB, MI->getDebugLoc(), TII->get(Vanilla::MOV), MI->getOperand(0).getReg()).addReg(Vanilla::R1);
+      }
+      MI->getOperand(0).setReg(Vanilla::R1);
+      MI->getOperand(1).setReg(Vanilla::R1);
+      transform++;
+      Changed=true;
+      continue;
+    }
+    else{
+      I++;
+    }
   }
-  return Changed;
+  return false;
 }
 
+bool VanillaVerifier::runOnMachineBasicBlock_addwait(MachineBasicBlock &MBB) {
+  const TargetInstrInfo *TII = Subtarget->getInstrInfo();
+  for (MachineBasicBlock::iterator I = MBB.begin(); I != MBB.end(); I++) {
+    if(I->getOpcode()==Vanilla::JALR && I->getOperand(0).isReg() && I->getOperand(0).getReg()==Vanilla::R2){
+      BuildMI(MBB, I, I->getDebugLoc(), TII->get(Vanilla::WAIT), Vanilla::R0).addReg(Vanilla::R0);
+    }
+  }
+  return true;
+}
